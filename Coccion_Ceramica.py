42c51651e0d5296461330e0c8a172332d7e0fe7c
marcelocardillo <34030323+marcelocardillo@users.noreply.github.com>
2023-09-19 14:17:21 -0300

    Coccion_Ceramica.py

--- Coccion_Ceramica.py
+++ Coccion_Ceramica.py
@@ -2,34 +2,47 @@ import streamlit as st
 import pandas as pd
 import altair as alt
 from collections import namedtuple
-import math
 
 # Título de la aplicación
 st.title('Gráfico de Temperatura de Cocción')
 
 # Sidebar con controles de entrada
 st.sidebar.header('Configuración de Datos')
-hora = st.sidebar.number_input('Hora de Cocción (en horas):', 0.0, 24.0, 0.0)
+hora_inicio = st.sidebar.text_input('Hora de inicio de Cocción (ejemplo: 14.40):')
+hora_fin = st.sidebar.text_input('Hora de fin de Cocción (ejemplo: 16.15):')
+
+# Convertir la hora ingresada en formato decimal
+def convertir_hora_decimal(hora_texto):
+    try:
+        horas, minutos = map(float, hora_texto.split('.'))
+        return horas + minutos / 60
+    except:
+        return None
+
+hora_inicio_decimal = convertir_hora_decimal(hora_inicio)
+hora_fin_decimal = convertir_hora_decimal(hora_fin)
+
 temperatura = st.sidebar.number_input('Temperatura de Cocción (en °C):', 0, 1500, 100)
 
 # Botón para agregar datos
 if st.sidebar.button('Agregar Datos'):
-    data = {'Hora': [hora], 'Temperatura': [temperatura]}
-    df = pd.DataFrame(data)
+    if hora_inicio_decimal is not None and hora_fin_decimal is not None:
+        data = {'Hora Inicio': [hora_inicio_decimal], 'Hora Fin': [hora_fin_decimal], 'Temperatura': [temperatura]}
+        df = pd.DataFrame(data)
 
-    # Si es la primera vez que se agrega un dato, creamos el DataFrame
-    if 'data' not in st.session_state:
-        st.session_state.data = df
-    else:
-        # Si ya existe el DataFrame, lo actualizamos agregando los nuevos datos
-        st.session_state.data = pd.concat([st.session_state.data, df], ignore_index=True)
+        # Si es la primera vez que se agrega un dato, creamos el DataFrame
+        if 'data' not in st.session_state:
+            st.session_state.data = df
+        else:
+            # Si ya existe el DataFrame, lo actualizamos agregando los nuevos datos
+            st.session_state.data = pd.concat([st.session_state.data, df], ignore_index=True)
 
 # Gráfico de línea con los datos ingresados
 if 'data' in st.session_state:
     st.header('Gráfico de Temperatura de Cocción')
     chart = alt.Chart(st.session_state.data).mark_line(color='#0068c9').encode(
-        x='Temperatura:Q',  # Cambio en el eje X
-        y='Hora:Q'          # Cambio en el eje Y
+        x='Hora Inicio:Q',
+        y='Temperatura:Q'
     ).properties(height=500, width=500)
     st.altair_chart(chart)
 
@@ -39,5 +52,5 @@ if 'data' in st.session_state:
         st.write(f'Media de Temperatura a partir de la segunda medición: {mean_temperature:.3f} °C')
 
     # Calcular el tiempo total de cocción en horas
-    total_cooking_time = st.session_state.data['Hora'].max() - st.session_state.data['Hora'].min()
+    total_cooking_time = st.session_state.data['Hora Fin'].max() - st.session_state.data['Hora Inicio'].min()
     st.write(f'Tiempo Total de Cocción en Horas: {total_cooking_time:.3f} horas')

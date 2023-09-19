e6c9b0ccbede6d61d982269cac97c718b1f91260
marcelocardillo <34030323+marcelocardillo@users.noreply.github.com>
2023-09-19 13:42:24 -0300

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,8 +9,8 @@ st.title('Gráfico de Temperatura de Cocción')
 
 # Sidebar con controles de entrada
 st.sidebar.header('Configuración de Datos')
-hora = st.sidebar.number_input('Hora de Cocción (en horas):', 0.0, 24.0, 0.0, step=0.5)
-temperatura = st.sidebar.number_input('Temperatura de Cocción (en °C):', 0, 300, 100)
+hora = st.sidebar.number_input('Hora de Cocción (en horas):', 0.0, 24.0, 0.0)
+temperatura = st.sidebar.number_input('Temperatura de Cocción (en °C):', 0, 1500, 100)
 
 # Botón para agregar datos
 if st.sidebar.button('Agregar Datos'):
@@ -24,11 +24,20 @@ if st.sidebar.button('Agregar Datos'):
         # Si ya existe el DataFrame, lo actualizamos agregando los nuevos datos
         st.session_state.data = pd.concat([st.session_state.data, df], ignore_index=True)
 
-# Gráfico de dispersión con los datos ingresados
+# Gráfico de línea con los datos ingresados
 if 'data' in st.session_state:
     st.header('Gráfico de Temperatura de Cocción')
-    chart = alt.Chart(st.session_state.data).mark_circle(color='#0068c9', opacity=0.5).encode(
+    chart = alt.Chart(st.session_state.data).mark_line(color='#0068c9').encode(
         x='Hora:Q',
         y='Temperatura:Q'
     ).properties(height=500, width=500)
     st.altair_chart(chart)
+
+    # Calcular la media de temperatura a partir de la segunda medición
+    if len(st.session_state.data) > 1:
+        mean_temperature = st.session_state.data['Temperatura'].mean()
+        st.write(f'Media de Temperatura a partir de la segunda medición: {mean_temperature:.3f} °C')
+
+    # Calcular el tiempo total de cocción en horas
+    total_cooking_time = st.session_state.data['Hora'].max() - st.session_state.data['Hora'].min()
+    st.write(f'Tiempo Total de Cocción en Horas: {total_cooking_time:.3f} horas')
